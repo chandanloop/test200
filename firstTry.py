f9639381684bc05e3552e8aff0e3dca0b3226a97
chandanloop <chandan.chandu.b@gmail.com>
2024-07-20 15:31:33 +0530

    “pythoncode”

---
+++ firstTry.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print("hi this is frist python code.")
+
+
+
+def main():
+    print("this is from main function.")
+    print("this is from main function.", np)
+  
+    
+main()
